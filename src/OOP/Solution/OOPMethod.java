62bb7ab093055b2b28a0773897a7360940f7e89a
Nadav-Levintov <DSP155@Gmail.com>
2016-06-01 22:19:28 +0300

    Finished Part A nothing much, just created the enum and Annotation

--- src/OOP/Solution/OOPMethod.java
+++ src/OOP/Solution/OOPMethod.java
@@ -1,7 +1,12 @@
 package OOP.Solution;
 
+import java.lang.annotation.ElementType;
+import java.lang.annotation.Target;
+
 /**
  * Created by Nadav on 01-Jun-16.
  */
+@Target(ElementType.METHOD)
 public @interface OOPMethod {
+    OOPModifier modifier() default OOPModifier.DEFAULT;
 }
